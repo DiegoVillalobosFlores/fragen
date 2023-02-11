03a296df92e7610d32614176c934b739244ff014
Diego Villalobos <diegovilalobos397@gmail.com>
2023-02-11 21:03:02 +0100

    Refactor button animations

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,6 +5,7 @@ import {motion, useAnimationControls} from "framer-motion";
 import styles from './Button.module.scss'
 import '@radix-ui/colors/amberDark.css'
 import '@radix-ui/colors/amberDark.css'
+import ButtonFont from "@/components/Button/ButtonFont";
 
 type Props = {
   children: ReactNode;
@@ -18,13 +19,15 @@ export default function Button({children, onClick}: Props) {
       layout
       onHoverStart={() => animationControls.start({width: '120%'})}
       onHoverEnd={() => animationControls.start({width: '25%'})}
-      whileHover={{fontSize: '20px'}}
+      whileHover={{scale: 1.1}}
       whileTap={{ scale: 0.9 }}
-      className={`${styles.root} dark-theme`}
+      initial={{scale: 1, width: 'auto'}}
+      exit={{scale: 1, width: 'auto'}}
+      className={`${styles.root} dark-theme ${ButtonFont.className}`}
       onClick={onClick}
     >
       {children}
-      <motion.div animate={animationControls} className={styles.underline}/>
+      <motion.div transition={{duration: 0.4}} animate={animationControls} className={styles.underline}/>
     </motion.button>
   )
 }
---
+++ src/components/Button/ButtonFont.tsx
@@ -0,0 +1,8 @@
+import {Ibarra_Real_Nova as Font} from "@next/font/google";
+
+const ButtonFont = Font({
+  subsets: ['latin'],
+  display: 'auto'
+})
+
+export default ButtonFont
