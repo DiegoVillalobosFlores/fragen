86721960acf1b15f64dd77faa67a667d2512a0e8
Diego Villalobos <diegovilalobos397@gmail.com>
2023-02-11 21:00:38 +0100

    Add motionVariables to variables.tsx

--- src/utils/variables.tsx
+++ src/utils/variables.tsx
@@ -1,1 +1,29 @@
 export const FRAGEN_GRAPH_NAME = 'Fragen'
+
+export const motionVariables = {
+  staggeredChildren: {
+    container: {
+      hidden: {opacity: 0},
+      show: {
+        opacity: 1,
+        transition: {
+          staggerChildren: 0.4
+        }
+      }
+    },
+    child:{
+      hidden: { opacity: 0 },
+      show: { opacity: 1 }
+    }
+  },
+  growingCorners: {
+    hoverStart: {
+      width: 40,
+      height: 40
+    },
+    hoverEnd: {
+      width: 20,
+      height: 20
+    }
+  }
+}
